52fe7e0403067a70ec15267960a25f3e95b74c97
Gaurav <gauravworks06@gmail.com>
2024-09-29 21:51:21 +0530

    Started dev work.

---
+++ src/main/java/com/gauravworks/spring_boot_microservice_course/SpringBootMicroserviceCourseApplication.java
@@ -0,0 +1,13 @@
+package com.gauravworks.spring_boot_microservice_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gauravworks/spring_boot_microservice_course/SpringBootMicroserviceCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauravworks.spring_boot_microservice_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
